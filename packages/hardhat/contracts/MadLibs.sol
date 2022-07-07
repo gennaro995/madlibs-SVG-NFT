cdbf87c63149f9470d74735bc6f1441c585ef77d
Gennaro Giordano <genny.slash@gmail.com>
2022-07-07 20:29:20 +0200

    fix

--- packages/hardhat/contracts/MadLibs.sol
+++ packages/hardhat/contracts/MadLibs.sol
@@ -12,13 +12,14 @@ contract YourCollectible is ERC721Enumerable, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
   Counters.Counter private _proposalIds;
-  uint8 public max_votes = 3;
+  uint8 public _maxVotes = 3;
+  MadLib[] public _madlibs;
+  using Strings for uint256;
 
   /**
   * @dev
   * Note: 
   *   - words -> parole della proposta
-  *   - votes -> voti totalizzati dalla proposta
   * Requirements:
   *   -
   *   -
@@ -28,8 +29,8 @@ contract YourCollectible is ERC721Enumerable, Ownable {
   struct Proposal{
     uint256 id;
     string[] words;
-    uint256 nvotes;
-    bool isProposal;
+    uint256 countVotes;
+    bool done;
   }
  
   /**
@@ -46,33 +47,26 @@ contract YourCollectible is ERC721Enumerable, Ownable {
   struct MadLib{ 
     uint256 id;
     string text;
-    uint8 nwords;
+    uint8 nBlanks;
     mapping(address => Proposal) proposals;
-    mapping(address => uint8) nvote;
+    mapping(address => uint8) addrVotes;
     bool closed;
   }
 
-  MadLib[] public madlibs;
-
-  event WordGuessed(address indexed sender, string word, uint8 index);
-
   
+
 
   constructor() ERC721("MadLibs onChain", "MLC") {
   }
 
-  function mintItem(string memory _text, uint8 _nwords) public onlyOwner returns (uint256) {
+  function mintItem(string memory _text, uint8 _nBlanks) public onlyOwner returns (uint256) {
     _tokenIds.increment();
-    _proposalIds.increment();
     uint256 id = _tokenIds.current();
     _mint(msg.sender, id);
-    //processText(_text, sessions[0]);
-    MadLib storage item2mint;
-    item2mint.id = id;
-    item2mint.text = _text;
-    item2mint.nwords = _nwords;
-    item2mint.closed = false;
-    //madlibs.push(item2mint); //TODO
+    _madlibs[id].id = id;
+    _madlibs[id].text = _text;
+    _madlibs[id].nBlanks = _nBlanks;
+    _madlibs[id].closed = false;
     return id;
   }
 
@@ -81,30 +75,87 @@ contract YourCollectible is ERC721Enumerable, Ownable {
   }*/
 
    function getProposal(uint _id, address _addr) public view returns(Proposal memory){
-    return madlibs[_id].proposals[_addr]; 
+    return _madlibs[_id].proposals[_addr]; 
   }
 
   /*function getProposals(uint _id, address _addr) public view returns(mapping(address => Proposal) memory){ //TODO
     return madlibs[_id].proposals; 
   }*/
 
-  function addProposal(uint _id, string memory _words) public { 
-    require (madlibs[_id].proposals[msg.sender].isProposal == false, "Player already has a proposal for this MadLib!");
-    Proposal memory proposal;
-    proposal.isProposal = true;
-    proposal.id = _tokenIds.current();
-    uint8 n = madlibs[_id].nwords;
+  function _addProposal(string[] memory _words) private{ 
+    uint256 id = _tokenIds.current();
+    require (_madlibs[id].proposals[msg.sender].done == false, "Player already has a proposal for this MadLib!");
+    _madlibs[id].proposals[msg.sender].done = true;
+    _proposalIds.increment();
+    _madlibs[id].proposals[msg.sender].id =  _proposalIds.current();
+    //uint256 nBlanks =  _madlibs[id].nBlanks;
 
-    for (uint i = 0; i < n; i++) {
-      proposal.words[i] = _words; // TODO si deve capire come far arrivale le parole
-    }
+    //for (uint i = 0; i < nBlanks; i++) {
+      _madlibs[id].proposals[msg.sender].words = _words;
+    //}
+  }
 
-    madlibs[_id].proposals[msg.sender] = proposal;
+  function voteProposal(address _idProposal) public{
+    uint256 id = _tokenIds.current();
+    require (_madlibs[id].closed == false, "Proposal must be for open/current MadLib!");
+    _madlibs[id].proposals[_idProposal].countVotes++;
+  }
 
+  function tokenURI(uint256 _id) public view override returns (string memory) {
+      require(_exists(_id), "not exist");
+      string memory title = string(abi.encodePacked('MadLibs #'));
+      string memory description = string(abi.encodePacked('This MabLibd Game onChain'));
+      string memory image = Base64.encode(bytes(generateSVGofTokenById(_id)));
+
+      return
+          string(
+              abi.encodePacked(
+                'data:application/json;base64,',
+                Base64.encode(
+                    bytes(
+                          abi.encodePacked(
+                              '{"name":"',
+                              title,
+                              '", "description":"',
+                              description,
+                              '", "external_url":"https://burnyboys.com/token/',
+                              _id.toString(),
+                              '", "owner":"',
+                              (uint160(ownerOf(_id))).toHexString(20),
+                              '", "image": "',
+                              'data:image/svg+xml;base64,',
+                              image,
+                              '"}'
+                          )
+                        )
+                    )
+              )
+          );
+  }
+
+  function generateSVGofTokenById(uint256 _id) internal view returns (string memory) {
+
+    string memory svg = string(abi.encodePacked(
+      '<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">',
+        renderTokenById(_id),
+      '</svg>'
+    ));
+
+    return svg;
+  }
+
+  function renderTokenById(uint256 _id) public view returns (string memory) {
+      return string(abi.encodePacked(
+        '<rect width="99%" height="99%" fill="white" />',
+        '<text x="15" y="30" style="fill:black;"> PIPPO' ,
+          '<tspan x="15" y="85">LICENSE PLATE: </tspan> PLUTO',
+        '</text>'
+      ));
+    
   }
 
   // find #x where x is the first letter of a type of word (a->adjective, v -> verb, n-> noun ecc) those will be the mad lips
-  function processText (string memory _at, MadLib storage session) private {
+  /*function processText (string memory _at, MadLib storage session) private {
     bytes memory whatBytes = bytes ("#");
     bytes memory whereBytes = bytes (_at);
 
@@ -127,7 +178,7 @@ contract YourCollectible is ERC721Enumerable, Ownable {
     }
     require (found);
 
-  }
+  }*/
 
 
 }
